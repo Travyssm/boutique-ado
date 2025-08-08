ea36672310415c0cffef2e42dd50e20e2b531461
Travyssm <travys.mills@hotmail.com>
2025-08-08 15:16:11 +0000

    updated settings.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """Handle Stripe webhooks"""
+    """Handle Stripe Webhooks"""
 
     def __init__(self, request):
         self.request = request
@@ -14,18 +14,18 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-    
+
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle the payment_intent.succeeded webhook from Stripe
+        Handle the payment_intent.succeeded webhook from stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-    
+
     def handle_payment_intent_payment_failed(self, event):
         """
-        Handle the payment_intent.payment_failed webhook from Stripe
+        Handle the payment_intent.payment_failed webhook from stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
